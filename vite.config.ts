a3a24660be0b540afa98eae9844ddc369a6461ef
wikkiboi <mofouyu@gmail.com>
2024-11-24 22:57:45 -0800

    Compile build test

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import Terminal from "vite-plugin-terminal";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Terminal({
-      console: "terminal",
-      output: ["terminal", "console"],
-    }),
-  ],
+  plugins: [react()],
 });
+
+// Terminal({
+//   console: "terminal",
+//   output: ["terminal", "console"],
+// }),
