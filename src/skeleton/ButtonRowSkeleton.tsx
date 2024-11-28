3ef313b26a9aa31c9c5e0c6045c9897224478a2a
wikkiboi <mofouyu@gmail.com>
2024-11-28 06:03:32 -0800

    Skeleton mobile fixes

--- src/skeleton/ButtonRowSkeleton.tsx
+++ src/skeleton/ButtonRowSkeleton.tsx
@@ -1,3 +1,3 @@
 export default function ButtonRowSkeleton() {
-  return <div className="skeleton w-1/3 h-6 mx-auto my-2"></div>;
+  return <div className="skeleton md:w-1/3 w-1/4 h-6 mx-auto my-2"></div>;
 }
--- src/skeleton/ImageSkeleton.tsx
+++ src/skeleton/ImageSkeleton.tsx
@@ -1,3 +1,5 @@
 export default function ImageSkeleton() {
-  return <div className="skeleton w-44 aspect-[7/10] mx-auto mb-2"></div>;
+  return (
+    <div className="skeleton md:w-44 w-28 aspect-[7/10] mx-auto mb-2"></div>
+  );
 }
