762c64fb83714bb845af0eb9dc92f82381425fe2
wikkiboi <mofouyu@gmail.com>
2024-11-28 06:23:59 -0800

    Short search results formatting

--- src/components/ShortSearchResults.tsx
+++ src/components/ShortSearchResults.tsx
@@ -16,21 +16,24 @@ export default function ShortSearchResults({
         <div className="bg-base-100 rounded-xl p-4">
           {searchData.map((search) => {
             let year;
-            if (search.year === undefined) {
+            if (search.year == undefined) {
               year = search.aired.from ? search.aired.from.slice(0, 4) : "";
             } else {
-              year = search.year;
+              year = search.year.toString();
             }
 
             return (
-              <div className="result-container" key={search.mal_id}>
-                <Link to={`/anime/${search.mal_id}`} className="result-card">
-                  <div className="result-image">
-                    <img src={search.images.webp.image_url} />
-                  </div>
-                  <div className="result-info">
-                    <h1>{search.title_english || search.title}</h1>
-                    <h2>{`(${search.type}, ${year})`}</h2>
+              <div className="flex" key={search.mal_id}>
+                <Link to={`/anime/${search.mal_id}`} className="flex mb-2">
+                  <img
+                    src={search.images.webp.image_url}
+                    className="w-8 aspect-[7/10] object-cover mr-2"
+                  />
+                  <div className="flex flex-col md:max-w-80 max-w-60">
+                    <h1 className="font-bold text-sm truncate">
+                      {search.title_english || search.title}
+                    </h1>
+                    <h2 className="text-xs">{`(${search.type}, ${year})`}</h2>
                   </div>
                 </Link>
               </div>
