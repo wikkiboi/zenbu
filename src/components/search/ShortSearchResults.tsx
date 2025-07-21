77b1609536dc98d51842de8cd8522b68f20f6e4c
wikkiboi <mofouyu@gmail.com>
2025-07-20 20:43:53 -0700

    Quick import line fix

--- src/components/search/ShortSearchResults.tsx
+++ src/components/search/ShortSearchResults.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import { Anime } from "../api/types";
+import { Anime } from "../../api/types";
 
 interface ShortSearchResultsProps {
   searchData?: Anime[];
